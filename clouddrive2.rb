119b05ecef60b050b2e91b30f89e3daf14272c45
cloud-fs <waytechcloudfs@gmail.com>
2025-02-01 18:36:04 +0800

    fixed typo

--- clouddrive2.rb
+++ clouddrive2.rb
@@ -11,7 +11,7 @@ class Clouddrive2 < Formula
         url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.8.7/clouddrive-2-macos-x86_64-0.8.7.tgz"
         sha256 "28ca1b0cb6d9dfcb407ada8fd83ac77bda02973cd8a6e4c4c8c577a0e80f5472"
     else
-        url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.8.7/clouddrive-2-macos-aarch64-0.8.7.tgz
+        url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.8.7/clouddrive-2-macos-aarch64-0.8.7.tgz"
         sha256 "c92abc697a42310b54f632458bb90968f0910eda995f2051bfd671de6dd89a6b"
   
     def install
