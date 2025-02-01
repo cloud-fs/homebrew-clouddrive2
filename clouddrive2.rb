684ff4c2d9499929ff2bb41ebcfac4795e3b9016
cloud-fs <waytechcloudfs@gmail.com>
2025-02-01 18:33:18 +0800

    first commit

---
+++ clouddrive2.rb
@@ -0,0 +1,31 @@
+class Clouddrive2 < Formula
+    desc "CloudDrive2"
+    homepage "https://www.clouddrive2.com"
+    version "0.8.7"
+    license "Proprietary"
+  
+    depends_on "macfuse"
+
+    
+    if Hardware::CPU.arm?
+        url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.8.7/clouddrive-2-macos-x86_64-0.8.7.tgz"
+        sha256 "28ca1b0cb6d9dfcb407ada8fd83ac77bda02973cd8a6e4c4c8c577a0e80f5472"
+    else
+        url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.8.7/clouddrive-2-macos-aarch64-0.8.7.tgz
+        sha256 "c92abc697a42310b54f632458bb90968f0910eda995f2051bfd671de6dd89a6b"
+  
+    def install
+        
+    end
+  
+    def caveats
+      <<~EOS
+        This formula installs a proprietary, closed-source CLI for CloudDrive.
+        Usage: clouddrive [options].
+      EOS
+    end
+  
+    test do
+      system "#{bin}/clouddrive", "--version"
+    end
+  end
