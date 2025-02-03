aea23266ce51ee266a30f8654ae0b32305197105
cloud-fs <waytechcloudfs@gmail.com>
2025-02-03 23:20:20 +0800

    complete the formula for current version 0.8.7

--- clouddrive2.rb
+++ clouddrive2.rb
@@ -1,31 +1,63 @@
 class Clouddrive2 < Formula
-    desc "CloudDrive2"
-    homepage "https://www.clouddrive2.com"
-    version "0.8.7"
-    license "Proprietary"
-  
-    depends_on "macfuse"
+  desc "CloudDrive2"
+  homepage "https://www.clouddrive2.com"
+  version "0.8.7"
+  license "Proprietary"
 
-    
-    if Hardware::CPU.arm?
-        url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.8.7/clouddrive-2-macos-x86_64-0.8.7.tgz"
-        sha256 "28ca1b0cb6d9dfcb407ada8fd83ac77bda02973cd8a6e4c4c8c577a0e80f5472"
-    else
-        url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.8.7/clouddrive-2-macos-aarch64-0.8.7.tgz"
-        sha256 "c92abc697a42310b54f632458bb90968f0910eda995f2051bfd671de6dd89a6b"
-    end
-  
-    def install
-        bin.install "clouddrive"
-    end
-  
-    def caveats
-      <<~EOS
-        Usage: clouddrive [options].
-      EOS
-    end
-  
-    test do
-      system "#{bin}/clouddrive", "--version"
-    end
+  #depends_on "macfuse"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.8.7/clouddrive-2-macos-aarch64-0.8.7.tgz"
+    sha256 "28ca1b0cb6d9dfcb407ada8fd83ac77bda02973cd8a6e4c4c8c577a0e80f5472"
+  else
+    url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.8.7/clouddrive-2-macos-x86_64-0.8.7.tgz"
+    sha256 "c92abc697a42310b54f632458bb90968f0910eda995f2051bfd671de6dd89a6b"
   end
+
+  def install
+    # Install binary and wwwroot into libexec
+    libexec.install "clouddrive", "wwwroot"
+
+    # Create a launch script for background execution in bin
+    (bin/"clouddrive-daemon").write <<~EOS
+      #!/bin/bash
+      cd #{libexec}
+      "./clouddrive"
+    EOS
+    chmod 0555, bin/"clouddrive-daemon"
+  end
+
+  service do
+    run [opt_bin/"clouddrive-daemon"]
+    run_type :immediate
+    keep_alive true
+    working_dir libexec
+    process_type :background
+    launch_only_once true
+  end
+  
+  def caveats
+    <<~EOS
+      CloudDrive2 requires macFUSE for full functionality.
+  
+      You can install macFUSE via Homebrew Cask:
+          brew install --cask macfuse
+      
+      Or download and install it directly from the official website:
+          https://macfuse.github.io/
+  
+      Once macFUSE is installed, you can start the CloudDrive2 service:
+          brew services start clouddrive2
+  
+      Then access the web interface at:
+          http://localhost:19798
+  
+      To stop the service:
+          brew services stop clouddrive2
+  
+      To uninstall CloudDrive2 entirely:
+          brew services stop clouddrive2
+          brew uninstall clouddrive2
+    EOS
+  end
+end
