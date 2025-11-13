b472153115aa58c285db7b67527bddbc942f1b18
cloud-fs <waytechcloudfs@gmail.com>
2025-11-14 01:10:42 +0800

    0.9.15

--- clouddrive2.rb
+++ clouddrive2.rb
@@ -1,17 +1,17 @@
 class Clouddrive2 < Formula
   desc "CloudDrive2"
   homepage "https://www.clouddrive2.com"
-  version "0.9.14"
+  version "0.9.15"
   license "Proprietary"
 
   #depends_on "macfuse"
   #shasum -a 256 file_path
   if Hardware::CPU.arm?
-    url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.9.14/clouddrive-2-macos-aarch64-0.9.14.tgz"
-    sha256 "4ade6027393a2630c5d57e101c1004a0cbe76eba44ae626962cb5c45909462ff"
+    url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.9.15/clouddrive-2-macos-aarch64-0.9.15.tgz"
+    sha256 "afe9481c411f8068bb442f935c3839a5d979de8b8389cc9f27f1e474282320c8"
   else
-    url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.9.14/clouddrive-2-macos-x86_64-0.9.14.tgz"
-    sha256 "214a6dc3c42c720fd47b14dddca42196a7664c41dfc7144d3677275fc28ceeee"
+    url "https://github.com/cloud-fs/cloud-fs.github.io/releases/download/v0.9.15/clouddrive-2-macos-x86_64-0.9.15.tgz"
+    sha256 "05c3e4b9e8c953a8d88a7be03fe510b5e7cc861bbbaba092c0c092b3d6f237fe"
   end
 
   def install
